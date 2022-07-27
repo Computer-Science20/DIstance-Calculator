ed69b27bb8da12ca5814a1e1c5eca52269311a3e
dhiyashah <dhiyashah2005@gmail.com>
2022-07-27 11:09:27 -0600

    Update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# Get the Input Values
+# Python Simple-Calc
+
+# Input
 x1 = float(input("Please enter a number: "))
 y1 = float(input("Please enter another number: "))
 x2 = float(input("Please enter another number: "))
 y2 = float(input("Please enter another number: "))
 
 # Process
-d = ((x2 - x1) ** 2 (y2 - y1) ** 2) ** 0.5
+total = ((x2 - x1) ** 2 (y2 - y1) ** 2) ** 0.5
 
-# Output Results
-print("The distance between the two points is" + str(d) + ".")
+# Output
+print("The sum of the two numbers is " + str(total) + ".")
